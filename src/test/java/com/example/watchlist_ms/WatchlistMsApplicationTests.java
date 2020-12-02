17b23240fb398c21ef2e149916c36ede91d239ad
Tom Bosmans <r0621531@student.thomasmore.be>
2020-12-02 13:33:46 +0100

    created bland spring project

---
+++ src/test/java/com/example/watchlist_ms/WatchlistMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.watchlist_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
